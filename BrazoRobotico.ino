4e93c0e89c62b91a4a47988796b40fe1e4bfe39d
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2017-11-01 08:54:32 -0500

    Added Servo Object

--- BrazoRobotico.ino
+++ BrazoRobotico.ino
@@ -1,6 +1,24 @@
+#include <Servo.h>
+
+#define pinBase 8
+#define pinArticulacion1 9
+#define pinArticulacion2 10
+#define pinGarra 11
+
+
+Servo Base;
+Servo Articulacion1;
+Servo Articulacion2;
+Servo Garra;
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
+ Base.attach(pinBase);
+ Articulacion1.attach(pinArticulacion1);
+ Articulacion2.attach(pinArticulacion2);
+ Garra.attach(pinGarra);
 }
 
 void loop() {
